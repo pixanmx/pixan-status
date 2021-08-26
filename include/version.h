2922cc650f6ac39b335af5c636656566bdac287f
ermiry <erick.salas@ermiry.com>
2021-08-25 23:02:59 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,9 +3,9 @@
 
 #define PIXAN_VERSION			"0.1"
 #define PIXAN_VERSION_NAME		"Version 0.1"
-#define PIXAN_VERSION_DATE		"17/07/2021"
-#define PIXAN_VERSION_TIME		"17:03 CST"
-#define PIXAN_VERSION_AUTHOR		"Erick Salas"
+#define PIXAN_VERSION_DATE		"25/08/2021"
+#define PIXAN_VERSION_TIME		"23:02 CST"
+#define PIXAN_VERSION_AUTHOR	"Erick Salas"
 
 // print full pixan version information
 extern void pixan_version_print_full (void);
